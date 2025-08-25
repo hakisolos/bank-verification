dfe623e33acc0b0aa9364c1c1f58e10517979012
hakisolos <maxwellexcel174@gmail.com>
2025-08-25 22:22:27 +0100

    esm issue fix

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from "express"
 import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 import path from "path"
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
